d81c7a71e796c95dacdcf133f8e5efd501200b6f
Jimi N <jimistk82@gmail.com>
2024-12-20 20:56:47 -0500

    apollo deps

---
+++ frontend/src/types.js
@@ -0,0 +1,40 @@
+/**
+ * @typedef {Object} ItemType
+ * @property {string} id
+ * @property {string} name
+ */
+
+
+/**
+ * @typedef {Object} Rarity
+ * @property {string} id
+ * @property {string} name
+ */
+
+/**
+ * @typedef {Object} Image
+ * @property {string} id
+ * @property {string} url
+ */
+
+/**
+ * @typedef {Object} Attribute
+ * @property {string} id
+ * @property {string} name
+ * @property {number} lowValue
+ * @property {number} highValue
+ * @property {AttributeGrouping} attributeGrouping
+ */
+
+/**
+ * @typedef {Object} AttributeGrouping
+ * @property {string} id
+ * @property {string} name
+ */
+
+/**
+ * @typedef {Object} Item
+ * @property {string} id
+ * @property {string} name
+ * @property {number} requiredLevel
+ */
